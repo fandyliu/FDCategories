54bab974cab152aae96c636da12536baae87e8ba
QianTuFD <qiantufd@QianTuLHdeiMac.local>
2016-08-15 18:16:54 +0800

    初步修改

---
+++ FDCategories/FDCategories.h
@@ -0,0 +1,19 @@
+//
+//  FDCategories.h
+//  FDCategories
+//
+//  Created by QianTuFD on 16/8/15.
+//  Copyright © 2016年 QianTuFD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDCategories.
+FOUNDATION_EXPORT double FDCategoriesVersionNumber;
+
+//! Project version string for FDCategories.
+FOUNDATION_EXPORT const unsigned char FDCategoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDCategories/PublicHeader.h>
+
+
